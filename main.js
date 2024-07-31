3945d130ad8df8a9322f70267b42fef2cdb0023c
nguyenvantung110 <nguyenvantung11102000@gmail.com>
2024-07-31 19:34:53 +0900

    update fetch data

--- main.js
+++ main.js
@@ -2,39 +2,29 @@
 const API_KEY = 'AIzaSyDsUflr7kTuKtWacS_P3jVvGn2mCxqkri4';
 // Thay thế bằng ID của Google Sheet của bạn
 const SHEET_ID = '19_mjN2Uv0Pm6-vvrq9C7brw3VvnJJODIkNo3eeLZOuY';
-
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(() => {
-        loadWords();
-    });
-}
+const SHEET_NAME = 'Sheet1'
 
 function loadWords() {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: 'Sheet1!A2:C',
-    }).then((response) => {
-        const words = response.result.values;
-        displayWords(words);
-    });
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => displayWords(data.values))
+    .catch(error => console.error('Error:', error));
 }
 
 function displayWords(words) {
-    const wordList = document.getElementById('wordList');
-    wordList.innerHTML = '';
     words.forEach((word) => {
-        const wordItem = document.createElement('div');
+        const wordItem = document.createElement('tr');
         wordItem.className = 'word-item';
         wordItem.innerHTML = `
-            <strong>${word[1]}</strong>: ${word[2]}
-            <button onclick="editWord('${word[0]}')">Sửa</button>
-            <button onclick="deleteWord('${word[0]}')">Xóa</button>
-        `;
+                <td>${word[0]}</td>
+                <td>${word[1]}</td>
+                <td>${word[2]}</td>
+                <td onclick="editWord('${word[0]}')">Sửa</td>
+                <td onclick="deleteWord('${word[0]}')">Xóa</td>
+            `;
         wordList.appendChild(wordItem);
     });
+
 }
 
 function addWord(event) {
@@ -64,4 +54,4 @@ function deleteWord(id) {
 
 document.getElementById('addWordForm').addEventListener('submit', addWord);
 
-gapi.load('client', initClient);
+gapi.load('client', loadWords);
